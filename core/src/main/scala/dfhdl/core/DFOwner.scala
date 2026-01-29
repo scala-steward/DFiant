b05d514a27dbf12fe9d75f977ba9aec8a980f8b2
Oron Port <oron.port@dfiant.works>
2026-01-30 01:20:27 +0200

    avoid anyval errors

--- core/src/main/scala/dfhdl/core/DFOwner.scala
+++ core/src/main/scala/dfhdl/core/DFOwner.scala
@@ -2,7 +2,7 @@ package dfhdl.core
 import dfhdl.internals.*
 import dfhdl.compiler.ir
 
-final class DFOwner[+T <: ir.DFOwner](val irValue: T | DFError) extends AnyVal with DFMember[T]
+final class DFOwner[+T <: ir.DFOwner](val irValue: T | DFError) extends DFMember[T]
 object DFOwner:
   extension [T <: ir.DFOwner](owner: T) def asFE: DFOwner[T] = DFOwner(owner)
 
